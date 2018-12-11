998a48733b1eb846338f62d4228fb58c2dce64b0
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-12-11 09:11:51 -0500

    clicking changes colors

--- app/src/App.js
+++ app/src/App.js
@@ -2,23 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Square extends Component {
-    render() {
-        return (<div className = { 'Square ' + ( this.props.color ) }></div>);
+function cycleColors(event) {
+    let element = event.target;
+    let cycle = {
+        'black': 'white',
+        'white': 'blue',
+        'blue': 'black' 
     };
+
+    let newName = 'Square '.concat(cycle[element.className.split(' ')[1]]);
+    element.className = newName;
 }
 
-
+class Square extends Component {
+    render() {
+        return (<div onClick = {cycleColors} className = { 'Square '.concat(( this.props.color )) }></div>);
+    };
+}
 
 class Grid extends Component {
     createSquares() {
         let squares = [];
-        let colors = ['red', 'black', 'white'];
 
         for (let i = 0; i < 8; i++) {
             for (let j = 0; j < 8; j++) {
                 
-                squares.push(<Square color = { colors[j % 3] } />);
+                squares.push(<Square color = 'white' />);
             }
         }
 
