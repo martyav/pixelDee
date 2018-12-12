bfd58e1b9f56a890e89636246c378d24c349ce93
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-12-12 04:56:39 -0500

    added colors, palettes, state

--- app/src/App.js
+++ app/src/App.js
@@ -2,21 +2,89 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function cycleColors(event) {
-    let element = event.target;
-    let cycle = {
-        'black': 'white',
-        'white': 'blue',
-        'blue': 'black' 
-    };
+class Palette {
+    static sonic() {
+        return {
+            'white': 'blue',
+            'blue': 'red',
+            'red': 'black',
+            'black': 'white',
+        }
+    }
 
-    let newName = 'Square '.concat(cycle[element.className.split(' ')[1]]);
-    element.className = newName;
+    static mario() {
+        return {
+            'white': 'tan',
+            'tan': 'brown',
+            'brown': 'red',
+            'red': 'white'
+        }
+    }
+
+    static grass() {
+        return {
+            'lime': 'green',
+            'green': 'yellow',
+            'yellow': 'tan',
+            'tan': 'lime'
+        }
+    }
+
+    static femme() {
+        return {
+            'pink': 'magenta',
+            'magenta': 'purple',
+            'purple': 'red',
+            'red': 'pink'
+        }
+    }
+
+    static printer() {
+        return {
+            'yellow': 'cyan',
+            'cyan': 'magenta',
+            'magenta': 'black',
+            'black': 'yellow'
+        }
+    }
+
+    static rgb() {
+        return {
+            'white': 'red',
+            'red': 'lime',
+            'lime': 'blue',
+            'blue': 'white'
+        }
+    }
 }
 
 class Square extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            palette: Palette.mario()
+        };
+
+        this.cycleColors = this.cycleColors.bind(this);
+    }
+
+    cycleColors(event) {
+        let element = event.target;
+        let cycle = this.state.palette;
+    
+        let newName = 'Square '.concat(cycle[element.className.split(' ')[1]]);
+        element.className = newName;
+    }
+
+    paletteSwap(newPalette) {
+        this.setState({
+           palette: newPalette 
+        });
+    }
+
     render() {
-        return (<div onClick = {cycleColors} className = { 'Square '.concat(( this.props.color )) }></div>);
+        return (<div onClick = {this.cycleColors} className = { 'Square '.concat(( this.props.color )) }></div>);
     };
 }
 
